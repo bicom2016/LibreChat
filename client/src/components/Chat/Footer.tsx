661169ec735468cf1a2a603d2ba24d9c82ed168b
bicom2016 <bi@comunicacaoaberta.com.br>
2025-05-19 08:51:37 -0300

    change footer and meta description

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -37,9 +37,9 @@ export default function Footer({ className }: { className?: string }) {
   const mainContentParts = (
     typeof config?.customFooter === 'string'
       ? config.customFooter
-      : '[LibreChat ' +
+      : '[ChatAI ' +
         Constants.VERSION +
-        '](https://librechat.ai) - ' +
+        '](https://en.wikipedia.org/wiki/Artificial_intelligence) - ' +
         localize('com_ui_latest_footer')
   ).split('|');
 
